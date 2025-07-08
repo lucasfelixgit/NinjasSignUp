5d7acdb17a5fb9a5b456a6e698eeb63a8856cb5b
lucasfelixgit <felixworkspace7@gmail.com>
2025-07-08 18:35:26 -0300

    create NinjaModel class and turn it into a database entity

---
+++ src/main/java/dev/java10x/NinjasSignUp/NinjaModel.java
@@ -0,0 +1,51 @@
+package dev.java10x.NinjasSignUp;
+
+import jakarta.persistence.*;
+
+//JPA = Java Persistence API
+//@Entity turns a class into a database entity.
+@Entity
+@Table(name = "tb_ninjas")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+
+    public NinjaModel(){
+    }
+
+    public NinjaModel(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
