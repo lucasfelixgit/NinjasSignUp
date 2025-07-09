a2565733204fca197aa6e6d4bce7f37990c18168
lucasfelixgit <felixworkspace7@gmail.com>
2025-07-09 16:33:42 -0300

    feat: add MissionRepository interface, introduction to JPA

---
+++ src/main/java/dev/java10x/NinjasSignUp/Mission/MissionRepository.java
@@ -0,0 +1,7 @@
+package dev.java10x.NinjasSignUp.Mission;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionRepository extends JpaRepository<MissionModel, Long> {
+
+}
