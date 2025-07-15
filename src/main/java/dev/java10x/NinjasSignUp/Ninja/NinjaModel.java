f9fd2e8ea4fd089099dc4cf4fbc45f18a165ef42
lucasfelixgit <felixworkspace7@gmail.com>
2025-07-15 15:06:29 -0300

    feat: add NinjaRepository

--- src/main/java/dev/java10x/NinjasSignUp/Ninja/NinjaModel.java
+++ src/main/java/dev/java10x/NinjasSignUp/Ninja/NinjaModel.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 //JPA = Java Persistence API
 //@Entity turns a class into a database entity.
 @Entity
---
+++ src/main/java/dev/java10x/NinjasSignUp/Ninja/NinjaRepository.java
@@ -0,0 +1,7 @@
+package dev.java10x.NinjasSignUp.Ninja;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+
+}
