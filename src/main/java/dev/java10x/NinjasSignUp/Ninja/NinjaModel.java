438c79f2649851cb8794a751fcf1842b9d99a131
lucasfelixgit <felixworkspace7@gmail.com>
2025-07-29 20:41:31 -0300

    refactor: add @ToString annotation to NinjaModel and MissionModel

--- src/main/java/dev/java10x/NinjasSignUp/Ninja/NinjaModel.java
+++ src/main/java/dev/java10x/NinjasSignUp/Ninja/NinjaModel.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 //JPA = Java Persistence API
 //@Entity turns a class into a database entity.
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@ToString(exclude = "mission")
 public class NinjaModel {
 
     @Id
